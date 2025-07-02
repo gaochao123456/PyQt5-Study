752edd2b956dc9a540349b204e27e9838188325a
Administrator <1033694664@qq.com>
2025-07-02 14:05:12 +0800

    我的第一个提交

---
+++ myFirstUi.py
@@ -0,0 +1,15 @@
+from PyQt5.Qt import *
+import sys
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Ui练习")
+        self.resize(500,500)
+        self.setUpUi()
+    def setUpUi(self):
+        pass
+if __name__ =="__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
